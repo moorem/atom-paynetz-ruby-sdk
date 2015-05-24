872e3026ea4b4ddca016824cff9d0384b9bf210c
Ankur Gupta <ankur@moorem.com>
2015-05-24 23:49:48 +0530

    Initial Commit

---
+++ lib/atom/paynetz/ruby/sdk.rb
@@ -0,0 +1,11 @@
+require "atom/paynetz/ruby/sdk/version"
+
+module Atom
+  module Paynetz
+    module Ruby
+      module Sdk
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/atom/paynetz/ruby/sdk/version.rb
@@ -0,0 +1,9 @@
+module Atom
+  module Paynetz
+    module Ruby
+      module Sdk
+        VERSION = "0.0.1"
+      end
+    end
+  end
+end
